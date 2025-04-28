284214c2c388c9daf59386247590abe4b2af47e8
S.M. ARIF HASAN <110717484+Arif1920002@users.noreply.github.com>
2025-04-28 22:42:30 +0600

    28 april 2025
    Signed-off-by: S.M. ARIF HASAN
    Signed-off-by: S.M. ARIF HASAN <110717484+Arif1920002@users.noreply.github.com>

---
+++ src/arithmeticOperation.java
@@ -0,0 +1,66 @@
+public class arithmeticOperation {
+
+    public static void main(String[] args) {
+
+        //Arithmetic Operation
+
+        int x = 10;
+        int y = 3;
+
+        z = x + y;
+        z = x - y;
+        z = x * y;
+        z = x / y;
+        z = x % y;
+
+        //Augmented Assignment Operator
+
+        int a = 20;
+        int b = 5;
+
+        a += b;
+        a -= b;
+        a *= b;
+        a /= b;
+        a %= b;
+
+        //Increment and Decrement Operator
+
+        int c = 10;
+
+        c++;
+        c--;
+        ++c;
+        --c;
+
+        //Logical Operator
+
+        int d = 10;
+        int e = 20;
+
+        boolean f = d < e;
+        boolean g = d > e;
+        boolean h = d <= e;
+        boolean i = d >= e;
+        boolean j = d == e;
+        boolean k = d != e;
+
+        //Order of Operations [PEMDAS]
+        /* P=Parentheses, E=Exponent, M=Multiplication, D=Division, A=Addition, S=Subtraction   */
+
+        int result;
+
+        // Example expression: (3 + 5) * 2 - 4 / 2
+        result = (3 + 5) * 2 - 4 / 2; // This follows PEMDAS
+
+        System.out.println("The result of the expression (3 + 5) * 2 - 4 / 2 is: " + result);
+
+
+        /*Parentheses: Calculate 3 + 5 first, which equals 8.
+        Multiplication: Multiply 8 * 2, resulting in 16.
+        Division: Calculate 4 / 2, which equals 2.
+        Subtraction: Finally, subtract 2 from 16, giving a final result of 14.*/
+
+
+    }
+}
