64b17183cbc6b70361e8b5dcd7aa1890adf5575b
S.M. ARIF HASAN <110717484+Arif1920002@users.noreply.github.com>
2025-04-30 23:01:09 +0600

    29 april 2025
    Signed-off-by: S.M. ARIF HASAN <110717484+Arif1920002@users.noreply.github.com>

--- src/RandomClass.java
+++ src/RandomClass.java
@@ -7,8 +7,13 @@ public class RandomClass {
         Random random = new Random();
 
         int number;
+        double number2;
 
         number = random.nextInt(1, 7);
         System.out.println(number);
+
+        number2 = random.nextDouble(1.1, 10.1);
+        System.out.println(number2);
+
     }
 }
