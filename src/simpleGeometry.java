54f475e7656fdb9352df19a63859d3bd886c78b7
S.M. ARIF HASAN <110717484+Arif1920002@users.noreply.github.com>
2025-05-04 05:04:56 +0600

    4 March 2025
    Signed-off-by: S.M. ARIF HASAN <110717484+Arif1920002@users.noreply.github.com>

---
+++ src/simpleGeometry.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+
+public class simpleGeometry {
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Area of a triangle = 0.5 * base * height
+        // Area of a rectangle = length * width
+        // Area of a circle = π * radius^2
+        // Area of a square = side * side
+        // Area of a cylinder = 2 * π * radius * height + 2 * π * radius^2
+
+
+        //triangle
+        System.out.println("For triangle");
+
+        double height, base, triangleArea;
+        System.out.println("Enter height: ");
+        height = scanner.nextDouble();
+        System.out.println("Enter base: ");
+        base = scanner.nextDouble();
+
+        triangleArea = 0.5 * base * height;
+        System.out.println("Area of the triangle: " + triangleArea);
+        System.out.println();
+
+
+        //rectangle
+        System.out.println("For rectangle");
+
+        double length, width, rectangleArea;
+        System.out.println("Enter length: ");
+        length = scanner.nextDouble();
+        System.out.println("Enter width: ");
+        width = scanner.nextDouble();
+
+        rectangleArea = 0.5 * length * width;
+        System.out.println("Area of the rectangle: " + rectangleArea);
+
+
+    }
+}
