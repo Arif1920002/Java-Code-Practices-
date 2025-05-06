795dc6525be484249cb26186ab166f5fea480c39
S.M. ARIF HASAN <110717484+Arif1920002@users.noreply.github.com>
2025-05-06 23:42:25 +0600

    Add stringDemo.java to print string length
    This code introduces a simple Java program that calculates and prints the length of a predefined string. It demonstrates basic string manipulation and output functionality.
    
    Signed-off-by: S.M. ARIF HASAN <110717484+Arif1920002@users.noreply.github.com>

---
+++ src/stringDemo.java
@@ -0,0 +1,11 @@
+public class stringDemo {
+
+    public static void main(String[] args) {
+
+        String name = "SM. Arif Hasan";
+
+        int length = name.length();
+
+        System.out.println("Length of the string: " + length);
+    }
+}
