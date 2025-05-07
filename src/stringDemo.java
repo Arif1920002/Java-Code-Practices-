31dde0057ca27ecb58cbd4094643e82161e770a6
S.M. ARIF HASAN <110717484+Arif1920002@users.noreply.github.com>
2025-05-08 00:04:17 +0600

    Add weightConverter class and extend stringDemo functionality
    Introduced a new weightConverter class to convert pounds to kilograms and added functionality to stringDemo for printing specific characters from the end of a string. Removed unused connectionManager component from aws.xml.
    
    Signed-off-by: S.M. ARIF HASAN <110717484+Arif1920002@users.noreply.github.com>

--- src/stringDemo.java
+++ src/stringDemo.java
@@ -7,5 +7,12 @@ public class stringDemo {
         int length = name.length();
 
         System.out.println("Length of the string: " + length);
+
+        System.out.println("First character: " + name.charAt(0));
+        System.out.println("Last character: " + name.charAt(length - 1));
+        System.out.println("Second last character: " + name.charAt(length - 2));
+        System.out.println("Third last character: " + name.charAt(length - 3));
+        System.out.println("Fourth last character: " + name.charAt(length - 4));
+        System.out.println("Fifth last character: " + name.charAt(length - 5));
     }
 }
---
+++ src/weightConverter.java
@@ -0,0 +1,7 @@
+public class weightConverter {
+    public static void main(String[] args) {
+        double pounds = 100;
+        double kilograms = pounds * 0.45359237;
+        System.out.println(pounds + " pounds is " + kilograms + " kilograms");
+    }
+}
